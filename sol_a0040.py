07f61fb4842e9d7788f4a963d8001fb44d358c94
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-03-29 19:10:51 +0530

    Update and rename sol_a009.Average_of_digits.py to sol_a0040.py

--- sol_a0040.py
+++ sol_a0040.py
@@ -1,8 +1,8 @@
-def avg_of_digits(number):
+def avg_of_5_digits(number):
     num = str(number)
     total = 0
     for i in num:
          total += float(i)
     return float(total/len(num))
 
-print(avg_of_digits(56565656))
+print(avg_of_5_digits(12345))
