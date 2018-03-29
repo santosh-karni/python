e1f3a5886f1bc50659809e3e7c8f683adb1fe60a
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-03-29 19:18:36 +0530

    Delete sol_a011.calculate gross salary.py

--- sol_a011.calculate gross salary.py
+++
@@ -1,9 +0,0 @@
-def calculate_gross_salary(basic_salary, dearness_allowance, house_rent):
-    gross_sal = basic_salary+(basic_salary*dearness_allowance)+house_rent
-    return gross_sal
-
-basic_salary = input("Enter basic salary : ")
-dearness_allowance = input("Enter dearness_allowance : ")
-house_rent = input("Enter house_rent  : ")
-
-print(calculate_gross_salary(basic_salary,dearness_allowance, house_rent))
