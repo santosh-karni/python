07977067b55f6b55034eddbef9f9a4903312d4f3
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-05 20:00:31 +0530

    Update sol_a0938.py

--- sol_a0938.py
+++ sol_a0938.py
@@ -1,10 +1,17 @@
+#a - First digit
+#b - Second digit
+#c - Third digit
+#d - Fourth digit
+#e - Fifth digit
+
+
 def make_number_from_5_digits(a,b,c,d,e):
     number = str(a)+str(b)+str(c)+str(d)+str(e)
     print type(number)
     return int(number)
 
 
-
+#user input
 a = int(input("Enter digit a : "))
 b = int(input("Enter digit b : "))
 c = int(input("Enter digit c : "))
