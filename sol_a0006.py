c941e94adbafece75759f31f5dd5410be4ef227e
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-05 14:11:40 +0530

    Update sol_a0006.py

--- sol_a0006.py
+++ sol_a0006.py
@@ -1,7 +1,13 @@
+#principle - Principle amount to be used for calculation
+#rate - Rate percentage to be used for calculation
+#years - Number of years to be used for calculation
+
 def get_si_amount(principle, rate, years):
     si = float((principle*rate*years)//100)
     return (si+principle)
 
+#take input from user
+#rate should be interms of decimal
 principle = input("Enter principle amount : ")
 rate = input("Enter interest rate : ")
 years = input("enter time in years : ")
