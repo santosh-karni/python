5d7f7f9c0ba5bda19e98159c443c83f656794aef
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-05 14:23:55 +0530

    Update sol_a0017.py

--- sol_a0017.py
+++ sol_a0017.py
@@ -1,7 +1,11 @@
+#cp - The cost price of the product.
+#profit - The profit percentage of the product.
+
 def get_profit(cp,profit_percent):
     profit = int(cp*profit_percent)
     return profit
 
+#input from user
 cp = int(input("Enter cost price :"))
 profit_percent = input("specify the percentage of profit :")
 
