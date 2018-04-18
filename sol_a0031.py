d0c27798958994badef4df2669c30a092952fbc1
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 17:57:18 +0530

    Update sol_a0031.py

--- sol_a0031.py
+++ sol_a0031.py
@@ -1,13 +1,27 @@
-#basic_salary - Basic Salary in Rupees
-#dearness_allowance - Dearness Allowance in percentage of Basic
-#house_rent - House Rent Allowance in Rupees
+#! /user/bin/python3
+"""Write a program when passed the Basic salary, Dearness Allowance (in percentage of Basic)
+ and House Rent Allowance in rupees, calculates and returns the gross salary,
+ which is the sum of all three."""
+
 
 def calculate_gross_salary(basic_salary, dearness_allowance, house_rent):
+    """Function to Calculate Gross Salary"""
     gross_sal = basic_salary+(basic_salary*dearness_allowance)+house_rent
     return gross_sal
 
-basic_salary = input("Enter basic salary : ")
-dearness_allowance = input("Enter dearness_allowance : ")
-house_rent = input("Enter house_rent  : ")
 
-print(calculate_gross_salary(basic_salary,dearness_allowance, house_rent))
+def main():
+    """main Function"""
+    try:
+        basic_salary = float(input("Enter basic salary : "))
+        dearness_allowance = float(input("Enter dearness_allowance : "))
+        house_rent = float(input("Enter house_rent  : "))
+
+        print(calculate_gross_salary(basic_salary, dearness_allowance, house_rent))
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you \
+                    are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
