46a0a950ae1627221f4470b0b2dd4315ac8cddec
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-19 00:42:08 +0530

    Update sol_a2303.py

--- sol_a2303.py
+++ sol_a2303.py
@@ -1,5 +1,22 @@
-#Find the product of 2 numbers
+#! /user/bin/python3
+""" Write a program to find the Product of two numbers"""
 
-def product(a,b):
-    return a*b
-print(product(200,3))
+
+def product(num_a, num_b):
+    """Function to find product of 2 numbers"""
+    return num_a*num_b
+
+
+def main():
+    """Main Function"""
+    try:
+        num_a = float(input("Enter A value : "))
+        num_b = float(input("Enter B value : "))
+        print(product(num_a, num_b))
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you "
+              "are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
