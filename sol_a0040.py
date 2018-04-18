a810737cebb911c7cb638ea67a2d2729c9fcdaf1
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 18:03:13 +0530

    Update sol_a0040.py

--- sol_a0040.py
+++ sol_a0040.py
@@ -1,10 +1,25 @@
-#Find aerage of digits of a 5 digit number
+#! /user/bin/python
+""" Write a program when passed a 5 digit number returns the average of the digits"""
+
 
 def avg_of_5_digits(number):
+    """Function to get average"""
     num = str(number)
     total = 0
     for i in num:
-         total += float(i)
+        total += float(i)
     return float(total/len(num))
 
-print(avg_of_5_digits(12345))
+
+def main():
+    """main Function"""
+    try:
+        number = input("Enter 5 digit number : ")
+        print(avg_of_5_digits(number))
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you"
+              " are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
