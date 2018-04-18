79e14ead441ea56621e4c0ab43d83f93276d069e
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 17:59:55 +0530

    Update sol_a0032.py

--- sol_a0032.py
+++ sol_a0032.py
@@ -1,10 +1,25 @@
-#km - KMs to be converted to milli meters.
+#! /user/bin/python3
+"""Program to convert kms to mms when passed value in KMs returns values in Millimeters.
+Your function will not be tested for negative values."""
 
-def convert_kms_to_mms(km):
-    converter = km *1000000
+
+def convert_kms_to_mms(kms):
+    """Function to convert kms to mms"""
+    converter = kms * 1000000
     return converter
 
-km = int(input("enter a value : "))
-a = convert_kms_to_mms(km)
 
-print km,"kms =", a,"mms"
+def main():
+    """Main Function"""
+    try:
+        kms = int(input("enter a value : "))
+        result = convert_kms_to_mms(kms)
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you \
+                    are inserting digits only")
+    finally:
+        print(kms, "kms =", result, "mms")
+
+
+if __name__ == '__main__':
+    main()
