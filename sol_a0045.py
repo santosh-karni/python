86fc41d3c8756795f40759667aa782eb9af66788
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-19 00:32:52 +0530

    Update sol_a0045.py

--- sol_a0045.py
+++ sol_a0045.py
@@ -1,8 +1,21 @@
-#Find the volume of a cube
-#side - Side of the cube
+#! /user/bn/python3
+""" Write a program when passed the side of a cube, returns the volume of the cube. """
+
 
 def cub_volume(side):
-    return side**3
+    """ Function to find Cube Volume"""
+    return float(side)**3
 
-side = input("Enter side of cube : ")
-print(cub_volume(side))
+
+def main():
+    """Main Function"""
+    try:
+        side = float(input("Enter side of cube : "))
+        print(cub_volume(side))
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you \
+                are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
