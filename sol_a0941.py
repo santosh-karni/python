08c6b6c66f7d576d99fcea4f89f4614d66f4f8ae
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-19 00:37:19 +0530

    Update sol_a0941.py

--- sol_a0941.py
+++ sol_a0941.py
@@ -1,11 +1,27 @@
-#Check if the number is even
-#num = number to be checked
+#! /use/bin/python3
+""" Write a program to check if a number is even or no. Return 0 if the number is even,
+ else return -1."""
+
 
 def is_even(num):
-    if num % 2 == 0:
-        return int(True)
+    """Function to check the number"""
+    if num <= 0:
+        print('invalid. Positive numbers are allowed')
+    elif num % 2 == 0:
+        print(int(False))
     else:
-        return int(False)
+        print(int(-True))
 
-num = int(input("Enter a number : "))
-print(is_even(num))
+
+def main():
+    """Main Function"""
+    try:
+        num = int(input("Enter a number : "))
+        is_even(num)
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you "
+              "are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
