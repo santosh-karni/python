a4d83a07117142c1889359d1426fdb6b221cfbe0
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 18:01:01 +0530

    Update sol_a0033.py

--- sol_a0033.py
+++ sol_a0033.py
@@ -1,8 +1,24 @@
-#kms - Kilometers to be converted into feet.
+#! /user/bin/python3
+""" Write a Program to convert KMs to Feet when passed values in KMs"""
+
 
 def convert_kms_to_feet(kms):
+    """Function to convert kms to feet"""
     converter = int(3280.8399*kms)
     return converter
 
-kms = int(input("Enter kilo meters :"))
-print(convert_kms_to_feet(kms))
+
+def main():
+    """Main Function"""
+    try:
+        kms = int(input("Enter kilometers :"))
+        result= convert_kms_to_feet(kms)
+    except ValueError:
+        print(print("Could not parse the input correctly. Please ensure that you \
+                are inserting digits only"))
+    finally:
+        print(kms, "KMs = ", result, "feet")
+
+
+if __name__ == '__main__':
+    main()
