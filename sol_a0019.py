8533fab74bda7ae231baac4788eea841f7e1d9cd
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 17:55:35 +0530

    Update sol_a0019.py

--- sol_a0019.py
+++ sol_a0019.py
@@ -1,17 +1,18 @@
-#radius - radius of the circle.
+#! /usr/bin/python3
+"""Write a Program when passed the radius returns the circumference. """
+
 
 def get_circumference(radius):
+    """Function to find the circumference"""
     circumference = 2*3.14*radius
     if radius < 1:
         circumference = 0
         print("enter a  positive value")
     else:
         return circumference
-#r = int(input("Enter the radius of a circle :"))
-a= get_circumference(20)
-b= get_circumference(20.3)
-c= get_circumference(-20)
 
-print(a)
-print(b)
-print(c)
+
+# r = int(input("Enter the radius of a circle :"))
+A = get_circumference(20)
+
+print(A)
