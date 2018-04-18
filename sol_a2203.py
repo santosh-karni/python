b11450b22d2589059b05db46c4fad80d9493397a
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-19 00:39:35 +0530

    Update sol_a2203.py

--- sol_a2203.py
+++ sol_a2203.py
@@ -1,19 +1,30 @@
-#Find the maximum of 3 numbers
-# a - First number
-#b - Second number
-#c - Third number
+#! /user/bin/python3
+""" Write a program takes two numbers as arguments and returns the largest of them."""
 
-def max_of_three(a,b,c):
-    if a > b and a > c:
-        return a
-    elif b > c and b > a:
-        return b
-    elif c > a and c > b:
-        return c
+
+def max_of_three(num_a, num_b, num_c):
+    """Function to find largest number """
+    if num_a > num_b and num_a > num_c:
+        print(num_a)
+    elif num_b > num_c and num_b > num_a:
+        print(num_b)
+    elif num_c > num_a and num_c > num_b:
+        print(num_c)
     else:
         print("all are equal")
-#user input
-a = int(input("Enter values of a : "))
-b = int(input("Enter value of b : "))
-c = int(input("Enter value of c : "))
-print(max_of_three(a,b,c))
+
+
+def main():
+    """Main Function"""
+    try:
+        num_a = float(input("Enter values of a : "))
+        num_b = float(input("Enter value of b : "))
+        num_c = float(input("Enter value of c : "))
+        max_of_three(num_a, num_b, num_c)
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you "
+              "are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
