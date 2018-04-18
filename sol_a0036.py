e399d8d41b8d67e5b26dd691921ae1d9abab4df6
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-18 18:02:14 +0530

    Update sol_a0036.py

--- sol_a0036.py
+++ sol_a0036.py
@@ -1,7 +1,25 @@
-#fht_temp - Fahrenheit temperature to be converted to Celsius.
+#! /user/bin/python3
+""" Write a program to Convert Fahrenheit to Celsius when Fahrenheit temperature is
+ specified"""
+
 
 def convert_fahrenheit_to_celsius(fth_temp):
-    converter =float(5/9.0*(fth_temp-32))
+    """Function to convert F to C"""
+    converter = float(5/9.0*(fth_temp-32))
     return converter
 
-print(convert_fahrenheit_to_celsius(32))
+def main():
+    """Main Function"""
+    try:
+        fth_temp = float(input("Temperature in Fahrenheit :"))
+        result = convert_fahrenheit_to_celsius(fth_temp)
+    except ValueError:
+        print(print("Could not parse the input correctly. Please ensure that you \
+                are inserting digits only"))
+    finally:
+        print(fth_temp, "F =", result, "C")
+
+
+if __name__ == '__main__':
+    main()
+
