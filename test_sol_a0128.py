de899e7542b3318c2453033bb79401c7dd9edaee
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-04-27 20:02:34 +0530

    Update test_sol_a0128.py

--- test_sol_a0128.py
+++ test_sol_a0128.py
@@ -4,7 +4,7 @@ import sol_a0128
 
 class TestZero(unittest.TestCase):
     def test_is_zero(self):
-        self.assertTrue(sol_a0128.is_zero(0), False)
+        self.assertTrue(sol_a0128.is_zero(0), None)
 
 
 if __name__ == '__main__':
