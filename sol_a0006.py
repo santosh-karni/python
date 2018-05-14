d3dc73f8f550f493a17a089d53c19235c7246881
santosh karni <37769282+santosh-karni@users.noreply.github.com>
2018-05-14 07:41:47 +0000

    Add files via upload

--- sol_a0006.py
+++ sol_a0006.py
@@ -1,15 +1,26 @@
-#principle - Principle amount to be used for calculation
-#rate - Rate percentage to be used for calculation
-#years - Number of years to be used for calculation
+#! /usr/bin/python3
+""" Program to get the simple interest when principle amount, rate and years are
+specified."""
+
 
 def get_si_amount(principle, rate, years):
-    si = float((principle*rate*years)//100)
-    return (si+principle)
+    """Function to calculate simple interest."""
 
-#take input from user
-#rate should be interms of decimal
-principle = input("Enter principle amount : ")
-rate = input("Enter interest rate : ")
-years = input("enter time in years : ")
+    simple_int = float((principle*rate*years)/100.0)
+    return simple_int+principle
 
-print(get_si_amount(principle, rate, years))
+
+def main():
+    """ Main function."""
+    try:
+        principle = float(input("Enter principle amount : "))
+        rate = float(input("Enter interest rate : "))
+        years = float(input("enter time in years : "))
+        print(get_si_amount(principle, rate, years))
+    except ValueError:
+        print("Could not parse the input correctly. Please ensure that you \
+                    are inserting digits only")
+
+
+if __name__ == '__main__':
+    main()
